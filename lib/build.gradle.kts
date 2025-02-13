dd0012f19ac7ecd41aaea47e35d4ab818017a049
Nikhil Biju <nikhil.b@keyvalue.systems>
2025-02-13 10:29:37 +0530

    fix: android build fix

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -115,9 +115,9 @@ dependencies {
 mavenPublishing {
 //    publishToMavenCentral(SonatypeHost.DEFAULT)
 //    // or when publishing to https://s01.oss.sonatype.org
-////    publishToMavenCentral(SonatypeHost.S01)
+//    publishToMavenCentral(SonatypeHost.S01)
 //    // or when publishing to https://central.sonatype.com/
-//    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
     signAllPublications()
 
